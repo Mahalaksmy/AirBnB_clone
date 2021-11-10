d1754ecace8e5a7c443f80fe7fe64c22cbde32bc
CarolinaLopera <carolinalopera08@gmail.com>
2021-11-10 12:53:06 -0500

    method __str__

--- models/base_model.py
+++ models/base_model.py
@@ -11,18 +11,25 @@ class BaseModel:
     Magic method or constructor
     """
     """def __init__(self, *args, **kwargs):"""
-    def __init__(self, id, created_at, updated_at):
+    # name = None
 
+    def __init__(self, id=None, created_at=None, updated_at=None, name=None):
+        '''Inicialization'''
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = created_at
+        self.name = name
+        # type(self).__name__ 
 
     def __str__(self):
         """ I do'nt understand """
-        pass
+        str = '[' + self.name + '] ' + '('
+        # str += self.id + ')'
+        # name.__class__.__name__
+        return str
 
     def save(self):
         self.updated_at = datetime.now()
 
-    def to_dict(self):
-        pass
+    # def to_dict(self):
+    #     pass
