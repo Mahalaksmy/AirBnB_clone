f3759d9a3a5c8b091287e529fe30edcb9467909e
CarolinaLopera <carolinalopera08@gmail.com>
2021-11-10 17:54:54 -0500

    Ready to_dict method

--- models/base_model.py
+++ models/base_model.py
@@ -13,12 +13,12 @@ class BaseModel:
     """
     """def __init__(self, *args, **kwargs):"""
 
-    def __init__(self, id=None, created_at=None, updated_at=None, name=None):
+    def __init__(self, id=None, created_at=None, updated_at=None):
         """Inicialization of the attributes"""
+        # self.name = name
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        self.name = name 
 
     def __str__(self):
         """This method return a string with the attributes"""
@@ -30,5 +30,8 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        # dic = self.__dict__
-        pass
+        dic = self.__dict__
+        dic["__class__"] = self.__class__.__name__
+        dic["created_at"] = self.created_at.isoformat()
+        dic["updated_at"] = self.updated_at.isoformat()
+        return dic
