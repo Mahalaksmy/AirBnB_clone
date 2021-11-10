d39438cf13d10cfb8c06ff60bb29b4f59696407a
CarolinaLopera <carolinalopera08@gmail.com>
2021-11-10 16:46:03 -0500

    Ready __str__ method

--- models/base_model.py
+++ models/base_model.py
@@ -6,30 +6,29 @@ Class principal
 import uuid
 from datetime import date, time, datetime
 
+
 class BaseModel:
     """
     Magic method or constructor
     """
     """def __init__(self, *args, **kwargs):"""
-    # name = None
 
     def __init__(self, id=None, created_at=None, updated_at=None, name=None):
-        '''Inicialization'''
+        """Inicialization of the attributes"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = created_at
-        self.name = name
-        # type(self).__name__ 
+        self.updated_at = datetime.now()
+        self.name = name 
 
     def __str__(self):
-        """ I do'nt understand """
-        str = '[' + self.__class__.__name__ + '] ' + '('
-        str += self.id + ')'
-        # name.__class__.__name__
-        return str
+        """This method return a string with the attributes"""
+        stri = '[' + self.__class__.__name__ + '] ' + '('
+        stri += self.id + ') ' + str(self.__dict__)
+        return stri
 
     def save(self):
         self.updated_at = datetime.now()
 
-    # def to_dict(self):
-    #     pass
+    def to_dict(self):
+        # dic = self.__dict__
+        pass
