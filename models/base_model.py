0a2cf6f2f0281c523fab02b121a2c1b2b4767571
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-11-10 17:46:13 -0500

    V2 Update methods dict

--- models/base_model.py
+++ models/base_model.py
@@ -6,23 +6,32 @@ Class principal
 import uuid
 from datetime import date, time, datetime
 
+
 class BaseModel:
     """
     Magic method or constructor
     """
     """def __init__(self, *args, **kwargs):"""
-    def __init__(self, id, created_at, updated_at):
 
+    def __init__(self, id=None, created_at=None, updated_at=None, name=None):
+        """Inicialization of the attributes"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = created_at
+        self.updated_at = datetime.now()
+        self.name = name
 
     def __str__(self):
-        """ I do'nt understand """
-        pass
+        """This method return a string with the attributes"""
+        stri = '[' + self.__class__.__name__ + '] ' + '('
+        stri += self.id + ') ' + str(self.__dict__)
+        return stri
 
     def save(self):
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        pass
+        dic = self.__dict__
+        dic[__class__] = self.__class__.__name__
+        dic[__class__] = self.created_at.isoformat()
+        dic[__class__] = self.updated_at.isoformat()
+        return dic
