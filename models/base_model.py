02baa4989523768b7c37d8c8b91be9fdd21ea76f
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-11-11 00:47:22 -0500

    V3 Update some attributes

--- models/base_model.py
+++ models/base_model.py
@@ -13,25 +13,26 @@ class BaseModel:
     """
     """def __init__(self, *args, **kwargs):"""
 
-    def __init__(self, id=None, created_at=None, updated_at=None, name=None):
+    def __init__(self, id=None, created_at=None, updated_at=None):
         """Inicialization of the attributes"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        self.name = name
+        self.updated_at = self.created_at
+        #self.name = name
 
     def __str__(self):
         """This method return a string with the attributes"""
-        stri = '[' + self.__class__.__name__ + '] ' + '('
-        stri += self.id + ') ' + str(self.__dict__)
-        return stri
+        """stri = '[' + self.__class__.__name__ + '] ' + '('
+        stri += self.id + ') ' + str(self.__dict__)"""
+
+        return f"[{self.__class__.__name__ }] ({self.id} {str(self.__dict__)}) "
 
     def save(self):
         self.updated_at = datetime.now()
 
     def to_dict(self):
         dic = self.__dict__
-        dic[__class__] = self.__class__.__name__
-        dic[__class__] = self.created_at.isoformat()
-        dic[__class__] = self.updated_at.isoformat()
+        dic["__class__"] = self.__class__.__name__
+        dic["created_at"] = self.created_at.isoformat()
+        dic["updated_at"] = self.updated_at.isoformat()
         return dic
