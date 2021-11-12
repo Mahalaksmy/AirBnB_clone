18cb7f1a33073d402555d21cf37fd4360258ebe5
Mahalaksmy <3354@holbertonschool.com>
2021-11-12 17:16:24 -0500

    Update branch

---
+++ console.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import cmd
+import sys
+
+class HBNBCommand(cmd.Cmd):
+    # ---- Creation de class-----
+
+    # ------ Change of the prompt for '(hbnb)'----
+    prompt = '(hbnb)'
+
+    # ------Method 'emptyline'----
+    def emptyline(self):
+        pass
+    
+    # ------Method 'quit' and man quit----
+    def do_quit(self,line):
+        sys.exit(1)
+    
+    def help_quit(self):
+        print("Syntax: quit")
+        print("--Quit command to exit the program")
+    
+    # ------Method 'EOF'----
+    def do_EOF(self,line):
+        "Exit ctrl + C"
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
