9bca7a14ba35093ee3b8cfada9d11b18b0b06527
Mahalaksmy <3354@holbertonschool.com>
2021-11-12 21:45:38 -0500

    Update pycosestyle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Class FileStorage"""
-import os, json
+import os
+import json
 from models.base_model import BaseModel
 from models.user import User
 from models.user import User
@@ -15,17 +16,17 @@ class FileStorage():
     """Class FileStorage"""
 
     __file_path = './file.json'
-    __objects =  {}
+    __objects = {}
 
     def all(self):
         """Returns the dictionary __objects"""
         return self.__objects
-    
+
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         key = '{}.{}'.format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
-        
+
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         save_dic = {}
