495719366a0dbbb319952dea9e9b6aa7aaaf2b6e
Mahalaksmy <3354@holbertonschool.com>
2021-11-12 21:08:08 -0500

    update console.py

--- console.py
+++ console.py
@@ -2,18 +2,25 @@
 
 import cmd
 import sys
+import models
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
 
 class HBNBCommand(cmd.Cmd):
-    # ---- Creation de class-----
+    """ ---- Creation de class-----"""
 
-    # ------ Change of the prompt for '(hbnb)'----
+    """------ Change of the prompt for '(hbnb)'----"""
     prompt = '(hbnb)'
 
-    # ------Method 'emptyline'----
+    """------Method 'emptyline'----""" 
     def emptyline(self):
         pass
     
-    # ------Method 'quit' and man quit----
+    """ ------Method 'quit' and man quit----"""
     def do_quit(self,line):
         sys.exit(1)
     
@@ -21,10 +28,32 @@ class HBNBCommand(cmd.Cmd):
         print("Syntax: quit")
         print("--Quit command to exit the program")
     
-    # ------Method 'EOF'----
+    """ ------Method 'EOF'----"""
     def do_EOF(self,line):
         "Exit ctrl + C"
         return True
+    
+    def do_destroy(self, args):
+        """
+        Method that delete an instance
+        """
+        divide_clas = args.split()
+        if not args:
+            print("** class name missing **")   
+        elif divide_clas[0] not in self.lista_clas:
+            print("** class doesn't exist **")
+        elif len(divide_clas) == 1:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(args[0], args[1])
+            instances = models.storage.all()
+
+            if key in instances:
+                del instances[key]
+                models.storage.save()
+            else:
+                print("** no instance found **")
+    
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
