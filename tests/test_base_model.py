84d4e4c948a04490d0ba8c6eb1d57e93a8c1b79e
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-11-14 15:47:39 -0500

    V1 Class testing update

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
+""" Testing for test base """
+import unittest
 from models.base_model import BaseModel
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+class Test_clase_m(unittest.TestCase):
+	""" Test for class mode """
+	Model = BaseModel()
+	def test_none(self):
+		
+		self.assertIsNone(None, self.Model.__dict__.values())
+	
+	def test_BaseM(self):
+		""" Test for testModel global """
+		model_id = BaseModel()
+		id = model_id.id
+		self.assertNotEqual(id, self.Model.id)
+
+
+if __name__ == '__main__':
+	unittest.main()
