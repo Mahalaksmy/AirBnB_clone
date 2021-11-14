aecbfdbffbdc1cc7edca95a6d45da8e8c04f5e77
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-11-14 10:29:30 -0500

    V3 updateP8 user class

--- models/user.py
+++ models/user.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""Class User
+"""
+Class User
 which inherits from
 """
-
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
     """ Create attributes for user """
-
     email = ""
     password = ""
     first_name = ""
