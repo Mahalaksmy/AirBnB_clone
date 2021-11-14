c078e1bec5822c11f7ca5f6654398db3cfc3f3a5
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-11-14 16:02:44 -0500

    V2 New directory

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Testing for test base """
+import unittest
+from models.base_model import BaseModel
+import models
+
+
+class Test_clase_m(unittest.TestCase):
+	""" Test for class mode """
+	Model = BaseModel()
+	def test_none(self):
+		
+		self.assertIsNone(None, self.Model.__dict__.values())
+	
+	def test_BaseM(self):
+		""" Test for testModel global """
+		model_id = BaseModel()
+		id = model_id.id
+		self.assertNotEqual(id, self.Model.id)
+
+
+if __name__ == '__main__':
+	unittest.main()
